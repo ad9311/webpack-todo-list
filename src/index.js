5f558875634d7e1c2f9f9561d6fbd1dfaf80b18c
Ángel Díaz <diazangel9311@gmail.com>
2021-08-17 21:53:38 -0500

    Set up array in index.js
    Add style to the page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('h1');
+const list = [
+  { description: 'Clean kitchen', completed: false, index: 0},
+  { description: 'Walk dog', completed: false, index: 1},
+  { description: 'Buy a mop', completed: false, index: 2},
+  { description: 'Pay bills', completed: false, index: 3}
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+function renderList() {
+  let svg = '<svg class="svg" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M12 18c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3zm0-9c1.657 0 3 1.343 3 3s-1.343 3-3 3-3-1.343-3-3 1.343-3 3-3z"/></svg>';
+  for(let i = 0; i < list.length; i++) {
+    const ul = document.getElementById('task-list');
+    const li = document.createElement('li');
+    li.innerHTML = `<input class="me-2" type="checkbox"></input><label class="flex-grow-1">${list[i]['description']}</label>${svg}`;
+    li.classList = 'list-row';
+    ul.appendChild(li);
+  }
 }
 
-document.body.appendChild(component());
+window.addEventListener('load', () => {
+  renderList();
+});
+
+
